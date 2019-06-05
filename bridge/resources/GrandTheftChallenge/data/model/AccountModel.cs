fe76422374a498bb15878d2a8a1bf25ad487e3ca
Xabi <xabier1989@gmail.com>
2019-06-05 20:54:02 +0200

    Añadida clase para mapear las cuentas de usuarios

---
+++ bridge/resources/GrandTheftChallenge/data/model/AccountModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GrandTheftChallenge.Data.Model
+{
+    public class AccountModel
+    {
+        public int Id { get; set; }
+        public string SocialName { get; set; }
+        public int State { get; set; }
+        public string LastLogged { get; set; }
+    }
+}
