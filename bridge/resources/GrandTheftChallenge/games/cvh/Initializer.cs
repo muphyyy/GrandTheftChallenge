44204607060ef4c5c76b1fec0bf0b8d16ae423b5
xabier1989 <xabier1989@gmail.com>
2019-06-14 10:31:19 +0200

    Cambio de estructura

--- bridge/resources/GrandTheftChallenge/games/cvh/Initializer.cs
+++ bridge/resources/GrandTheftChallenge/games/cvh/Initializer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using GTANetworkAPI;
 
-namespace GrandTheftChallenge.cvh
+namespace GrandTheftChallenge.Games.CVH
 {
     public class Initializer : Script
     {
